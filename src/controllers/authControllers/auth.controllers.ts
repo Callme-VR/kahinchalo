2178a319e2b03e465562e43b096cab0cb6f30cf9
Callme-VR <176268004+Callme-VR@users.noreply.github.com>
2026-02-26 00:47:37 +0530

    user api created sucessfully

--- src/controllers/authControllers/auth.controllers.ts
+++ src/controllers/authControllers/auth.controllers.ts
@@ -25,8 +25,6 @@ export const registerUser = async (req: Request, res: Response): Promise<void> =
         password: hashedPassword, // return only for testing (remove in production)
       },
     });
-    console.log("201 API created successfully");
-
   } catch (error) {
     console.error("Error registering user:", error);
     res.status(500).json({ message: "Internal server error" });
