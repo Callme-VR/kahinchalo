6b1bcd264208c15f8c280cbee0c03e33460527e1
Callme-VR <176268004+Callme-VR@users.noreply.github.com>
2026-02-27 01:39:00 +0530

    add

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-// import cors from "cors";
-// import morgan from "morgan";
-// import helmet from "helmet";
+import cors from "cors";
+import morgan from "morgan";
+import helmet from "helmet";
 import authRoutes from "./routes/authRoutes/auth.routes";
 import userRoutes from "./routes/userRoutes/user.routes";
 import vendorRoutes from "./routes/vendorRoutes/vendor.routes";
@@ -18,9 +18,9 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-// app.use(helmet());
-// app.use(cors());
-// app.use(morgan("dev"));
+app.use(helmet());
+app.use(cors());
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
